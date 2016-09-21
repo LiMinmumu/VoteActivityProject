5c92a87c05f1512f476a5c5c4f3c40b82e295a6a
limin <1435292190@qq.com>
2016-09-21 18:39:33 +0800

    中秋投票活动页面最终版

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,25 @@ var router = express.Router();
 
 
 /*中秋节投票活动首页*/
-router.get('/vote',function(re,res,next){
-  res.render('workv2/index')
-});
-  /*注册页面*/
-router.get('/register',function(re,res,next){
-  res.render('workv2/register')
-});
-  /*参加活动的用户列表页*/
-router.get('/list',function(re,res,next){
-  res.render('workv2/ranklist')
-});
-  /*个人展示页*/
-router.get('/profile',function(re,res,next){
-  res.render('workv2/profile')
-});
+  router.get('/vote',function(req,res,next){
+    res.render('workv2/index')
+  });
+    /*注册页面*/
+  router.get('/register',function(req,res,next){
+    res.render('workv2/register')
+  });
+    /*参加活动的用户列表页*/
+  router.get('/list',function(req,res,next){
+    res.render('workv2/ranklist')
+  });
+    /*个人展示页*/
+  router.get('/profile',function(req,res,next){
+    res.render('workv2/profile')
+  });
+
+/*公益活动详情页*/
+  router.get("/detail",function(req,res,next){
+    res.render('workv2/detail')
+  });
+
 module.exports = router;
